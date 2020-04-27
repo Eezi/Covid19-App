a4f08145de615fde175ab5ef9b85e6fff545cfe7
Eezi <tomi.a.salo@outlook.com>
2020-04-27 20:19:22 +0300

    Globaali data saatu esitettyä sivulle.

---
+++ src/components/Cards/Cards.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { Card, CardContent, Typography, Grid, StylesProvider } from '@material-ui/core';
+import styles from './Cards.module.css';
+import CountUp from 'react-countup';
+
+const Cards = ( {data: { confirmed, recovered, deaths, lastUpdate }}) => {
+    console.log(deaths)
+    if(!confirmed) {
+        return 'Loading...'
+    }
+
+    return(
+        <div className={styles.container}>
+            <Grid container spacing={3} justify="center">
+                <Grid item component={Card}>
+                    <CardContent>
+                        <Typography color="textSecondary" gutterBottom>Infected</Typography>
+                        <Typography variant="h5">
+                            <CountUp start={0} end={confirmed.value} duration={2.5} separator="," />
+                        </Typography>
+                        <Typography color="textSecondary">DATE</Typography>
+                        <Typography variant="body2">Number of active cases of Corona</Typography>
+                    </CardContent>
+                </Grid>
+                <Grid item component={Card}>
+                    <CardContent>
+                        <Typography color="textSecondary" gutterBottom>Recovered</Typography>
+                        <Typography variant="h5">
+                            <CountUp start={0} end={recovered.value} duration={2.5} separator="," />
+                        </Typography>
+                        <Typography color="textSecondary">DATE</Typography>
+                        <Typography variant="body2">Number of recovered from Corona</Typography>
+                    </CardContent>
+                </Grid>
+                <Grid item component={Card}>
+                    <CardContent>
+                        <Typography color="textSecondary" gutterBottom>Deaths</Typography>
+                        <Typography variant="h5">
+                            <CountUp start={0} end={deaths.value} duration={2.5} separator="," />
+                        </Typography>
+                        <Typography color="textSecondary">DATE</Typography>
+                        <Typography variant="body2">Number deaths caused by Corona</Typography>
+                    </CardContent>
+                </Grid>
+
+            </Grid>
+
+        </div>
+    )
+}
+
+export default Cards;
---
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ContryPicker = () => {
+    return(
+        <h1>ContryPicker </h1>
+    )
+}
+
+export default ContryPicker;
